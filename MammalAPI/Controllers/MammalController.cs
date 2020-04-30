c6788191aaaa72830766a8e12124c7dc7fd2c017
Murat <m.kugic@gmail.com>
2020-04-30 09:06:39 +0200

    Updated controller
    Need Service to be ready to be able to create the whole contorller.
    
    Shell is completed.

--- MammalAPI/Controllers/MammalController.cs
+++ MammalAPI/Controllers/MammalController.cs
@@ -1,11 +1,27 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MammalAPI.Controllers
 {
-    public class MammalController
+    [ApiController]
+    [Route("api/v1.0/[controller]")]
+    public class MammalController : ControllerBase
     {
+        // Need to DI repository into controller to be used to call the "GetAllMammals" method
+        // Need to initialize 
+        public MammalController()
+        {
+
+        }
+
+        // Need to return Ok and await service that is not yet created.
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return null;
+        }
     }
 }
