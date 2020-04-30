66fa4bb9487c939fa2716683a3e2b5376da5f090
Fredrik Odén <fredrikoden87@gmail.com>
2020-04-30 12:45:24 +0200

    Updated Startup.cs to use our own pipeline

--- MammalAPI/Startup.cs
+++ MammalAPI/Startup.cs
@@ -16,6 +16,7 @@ namespace MammalAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(options => options.EnableEndpointRouting=false).SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,16 +26,18 @@ namespace MammalAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseMvc();
 
-            app.UseRouting();
+            // Gammalt som auto-skapades
+            // app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            // app.UseEndpoints(endpoints =>
+            // {
+            //     endpoints.MapGet("/", async context =>
+            //     {
+            //         await context.Response.WriteAsync("Hello World!");
+            //     });
+            // });
         }
     }
 }
