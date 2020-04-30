b3d442d003635ae0343ed2457e693bff2ac20e07
AntonNorgren <56293438+AntonNorgren@users.noreply.github.com>
2020-04-30 13:28:51 +0200

    Added FakeModel
    Created folder and added FakeMammal.cs

---
+++ MammalAPI/Models/FakeMammal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MammalAPI.Models
+{
+    public class FakeMammal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
