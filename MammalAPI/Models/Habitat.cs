4cb7fdb2962548e743c45ed020d7c30abed27285
zorkii <micael.wollter@gmail.com>
2020-05-05 09:50:47 +0200

    Created Habitat DB Table

---
+++ MammalAPI/Models/Habitat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MammalAPI.Models
+{
+    public class Habitat
+    {
+        [Key]
+        public int HabitatID { get; set; }
+        public string Name { get; set; }
+
+    }
+}
