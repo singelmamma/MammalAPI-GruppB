177f46612bcce4c15312b0af50941abf2f543fe5
Murat <m.kugic@gmail.com>
2020-05-05 10:17:14 +0200

    Added key attribute

--- MammalAPI/Models/MammalHabitat.cs
+++ MammalAPI/Models/MammalHabitat.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MammalAPI.Models
 {
     public class MammalHabitat
     {
+        [Key]
         public int Id { get; set; }
         public int AnimalId { get; set; }
         public int HabitatId { get; set; }
