881550f9235edc0eb10ddfc2d3b9066b5b912fc7
AntonNorgren <56293438+AntonNorgren@users.noreply.github.com>
2020-05-05 09:58:05 +0200

    Update Mammal.cs

--- MammalAPI/Models/Mammal.cs
+++ MammalAPI/Models/Mammal.cs
@@ -7,5 +7,13 @@ namespace MammalAPI.Models
 {
     public class Mammal
     {
+        public int MammalID { get; set; }
+        public string Name { get; set; }
+        public double Length { get; set; }
+        public double Weight { get; set; }
+        public string LatinName { get; set; }
+        public int Lifespan { get; set; }
+        public int FmailyID { get; set; }
+
     }
 }
