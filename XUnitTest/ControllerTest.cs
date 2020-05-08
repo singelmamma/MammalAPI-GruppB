9dc2effbae0f564376e5639431d3363716131015
Fredrik Odén <fredrikoden87@gmail.com>
2020-05-08 11:56:15 +0200

    Added first test that doesn't work

--- XUnitTest/ControllerTest.cs
+++ XUnitTest/ControllerTest.cs
@@ -6,17 +6,57 @@ using MammalAPI;
 using MammalAPI.Models;
 using MammalAPI.Controllers;
 using MammalAPI.Services;
+using Microsoft.AspNetCore.Mvc;
 using MammalAPI.Context;
-
+using Moq;
+using System.Threading.Tasks;
+using System.Net.Http;
 
 namespace XUnitTest
 {
     public class ControllerTest
     {
         [Fact]
-        public void Test1()
+        public void GetReturnsMammal()
         {
+            
+        }
 
+        [Fact]
+        public async Task Test1()
+        {
+            // Arrange
+            var mockRepo = new Mock<IMammalRepository>();
+            mockRepo.Setup(repo => repo.GetAllMammals())
+                    .ReturnsAsync(GetTestSessions());
+            var controller = new MammalController(mockRepo.Object);
+
+            // Act
+            var result = await controller.Get();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<IList<Mammal>>(
+                        viewResult.ViewData.Model);
+            Assert.Equal(2, model.Count());
+        }
+        
+        private List<Mammal> GetTestSessions()
+        {
+            var sessions = new List<Mammal>();
+            sessions.Add(new Mammal()
+            {
+                MammalId = 1,
+                Name = "Test Mammal One",
+                LatinName = "Bapa latin apa"
+            });
+            sessions.Add(new Mammal()
+            {
+                MammalId = 2,
+                Name = "Test Mammal Two",
+                LatinName = "Latin kanske med"
+            });
+            return sessions;
         }
     }
 }
