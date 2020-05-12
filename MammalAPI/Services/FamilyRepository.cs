d08ccfdb0ef2f6c7c7b4c9cf5cc5b8960902f035
Zorkii <56868345+aohzork@users.noreply.github.com>
2020-05-12 12:44:53 +0200

    Create FamilyRepository.cs

---
+++ MammalAPI/Services/FamilyRepository.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MammalAPI.Context;
+using MammalAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace MammalAPI.Services
+{
+    class FamilyRepository : IFamilyRepository
+    {
+        private readonly DBContext _dBContext;
+
+        public FamilyRepository(DBContext dBContext)
+        {
+            _dBContext = dBContext;
+        }
+
+        public async Task<Family> GetFamilyByName(string name)
+        {
+            
+        }
+
+        public async Task<Family> GetFamilyBy(int id)
+        {
+
+        }
+    }
+}
