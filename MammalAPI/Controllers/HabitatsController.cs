e9184082d516c76471b3b43aa4b415e34fca1a65
AntonNorgren <56293438+AntonNorgren@users.noreply.github.com>
2020-05-12 10:22:13 +0200

    added controller file

---
+++ MammalAPI/Controllers/HabitatsController.cs
@@ -0,0 +1,24 @@
+﻿using MammalAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace MammalAPI.Controllers
+{
+    [ApiController]
+    [Route("api/v1.0/[controller]")]
+    public class HabitatsController : ControllerBase
+    {
+
+        private readonly IHabitatRepository _habitatRepository;
+
+        public HabitatController(IHabitatRepository habitatRepository)
+        {
+            _habitatRepository = habitatRepository;
+        }
+
+    }
+}
