d27c24f9de75a3faae2336e73eee54adfe9d1236
Murat <m.kugic@gmail.com>
2020-05-12 14:06:49 +0200

    Added Murat@DESKTOP-GO4PDLN MINGW64 /c/dev/backend/git/MammalAPI-GruppB (MGB-57-GetMammalsByHabitatId)

--- MammalAPI/Services/IMammalRepository.cs
+++ MammalAPI/Services/IMammalRepository.cs
@@ -10,5 +10,6 @@ namespace MammalAPI.Services
         Task<List<Mammal>> GetAllMammals();
         Task<Mammal> GetMammalById(int id);
         Task<List<IdNameDTO>> GetMammalsByHabitat(string habitatName);
+        Task<List<IdNameDTO>> GetMammalsByHabitatId(int id);
     }
 }
