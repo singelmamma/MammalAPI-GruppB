917b63476ff1f366d91be3d3a75055c5aecce566
Fredrik Odén <fredrikoden87@gmail.com>
2020-05-13 15:27:20 +0200

    First test is working

--- XUnitTest/ControllerTest.cs
+++ XUnitTest/ControllerTest.cs
@@ -8,21 +8,36 @@ using MammalAPI.Controllers;
 using MammalAPI.Services;
 using Microsoft.AspNetCore.Mvc;
 using MammalAPI.Context;
+using Microsoft.Extensions.Logging;
 using Moq;
 using System.Threading.Tasks;
 using System.Net.Http;
+using Microsoft.EntityFrameworkCore;
+using Moq.EntityFrameworkCore;
 
 namespace XUnitTest
 {
     public class ControllerTest
     {
         [Fact]
-        public void GetReturnsMammal()
+        public void GetHabitatByName_FourDifferentHabitats_Id2()
         {
+            // Arrange
+            var habitatName = "East";
+            var habitats = GetMoqHabitats();
+            var contextMock = new Mock<DBContext>();
+            contextMock.Setup(h => h.Habitats).ReturnsDbSet(habitats);
+            var logger = Mock.Of<ILogger<HabitatRepository>>();
             
+            var habitatRepository = new HabitatRepository(contextMock.Object, logger);
+
+            // Act
+            var testResult = habitatRepository.GetHabitatByName(habitatName);
+
+            // Assert
+            Assert.Equal(habitatName, testResult.Result.Name);
         }
 
-        [Fact]
         public async Task Test1()
         {
             // Arrange
@@ -39,20 +54,6 @@ namespace XUnitTest
             var model = Assert.IsAssignableFrom<IList<Mammal>>(
                         viewResult.ViewData.Model);
             Assert.Equal(2, model.Count());
-        }
-
-        [Fact]
-        public async Task MammalControllerReturnsCorrectMammalGivenId()
-        {
-            // Arrange
-            var mockRepo = new Mock<IMammalRepository>();
-            var controller = new MammalController(mockRepo.Object);
-
-            // Act
-            var result = await controller.GetMammalById(1);
-
-            // Assert
-            Assert.NotEmpty(result.ToString());
         }
         
         private List<Mammal> GetTestSessions()
@@ -72,5 +73,36 @@ namespace XUnitTest
             });
             return sessions;
         }
+
+        private IList<Habitat> GetMoqHabitats()
+        {
+            var habitats = new List<Habitat>();
+            habitats.Add(new Habitat
+            {
+                HabitatID = 1,
+                Name = "North",
+                MammalHabitats = null
+            });
+            habitats.Add(new Habitat
+            {
+                HabitatID = 2,
+                Name = "East",
+                MammalHabitats = null
+            });
+            habitats.Add(new Habitat
+            {
+                HabitatID = 3,
+                Name = "South",
+                MammalHabitats = null
+            });
+            habitats.Add(new Habitat
+            {
+                HabitatID = 4,
+                Name = "West",
+                MammalHabitats = null
+            });
+            return habitats;
+        }
+
     }
 }
