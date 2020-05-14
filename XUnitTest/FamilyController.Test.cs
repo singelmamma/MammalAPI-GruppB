c4915352edaf0b05acca69fe6dddcde064df9d49
Zorkii <56868345+aohzork@users.noreply.github.com>
2020-05-14 23:45:10 +0200

    minor adjustments

--- XUnitTest/FamilyController.Test.cs
+++ XUnitTest/FamilyController.Test.cs
@@ -18,9 +18,6 @@ namespace XUnitTest
 {
     public class FamilyControllerTest
     {
-        /*https://docs.microsoft.com/en-us/aspnet/web-api/overview/testing-and-debugging/unit-testing-controllers-in-web-api#testing-actions-that-return-httpresponsemessage
-         https://stackoverflow.com/questions/50801094/how-to-get-the-values-from-a-taskiactionresult-returned-through-an-api-for-uni*/
-
         [Fact]
         public async Task GetReturnsFamilyById_ExpectCorrectIdAsync()
         {
