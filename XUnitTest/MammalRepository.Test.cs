b9f00ddc72b8304b03fc5717285b4de626d24a7a
Zorkii <56868345+aohzork@users.noreply.github.com>
2020-05-15 00:41:09 +0200

    Update MammalRepository.Test.cs

--- XUnitTest/MammalRepository.Test.cs
+++ XUnitTest/MammalRepository.Test.cs
@@ -11,6 +11,8 @@ using MammalAPI.Context;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Moq.EntityFrameworkCore;
+using MammalAPI.DTO;
+using System.Threading.Tasks;
 
 namespace XUnitTest
 {
@@ -51,6 +53,48 @@ namespace XUnitTest
                 LatinName = "Latin kanske med"
             });
             return sessions;
+        }
+
+        [Fact]
+        public void GetAllMammals_TwoTestMammals_EqualListsOfMammals()
+        {
+            // Arrange
+            var contextMock = new Mock<IMammalRepository>();
+            contextMock.Setup(m => m.GetAllMammals()).Returns(GetTestAllMammals());
+            var logger = Mock.Of<ILogger<MammalRepository>>();
+
+            var mammalRepository = new MammalRepository(contextMock.Object, logger);
+
+            // Act
+            var expected = mammalRepository.GetAllMammals();
+
+            // Assert
+           // Assert.Equal(2, expected.Result.MammalId);
+        }
+
+        private async Task<List<MammalDTO>> GetTestAllMammals()
+        {
+            List<MammalsDTO> items = new List<MammalsDTO>();
+            items.Add(new MammalsDTO
+            {
+                MammalId = 1,
+                Name = "Grey Whale",
+                LatinName = "Latin name1",
+                Length = 250,
+                Weight = 30000
+            });
+            items.Add(new MammalsDTO
+            {
+                MammalId = 2,
+                Name = "Pink Whale",
+                LatinName = "Latin name2",
+                Length = 750,
+                Weight = 800000
+            });
+
+            return Task.Run(() => items);
+
+           // return items;
         }
     }
 }
