4a4154beedaba837550fa275e4a9915b13954a70
Zorkii <56868345+aohzork@users.noreply.github.com>
2020-05-14 23:42:32 +0200

    updated to check everything

--- XUnitTest/FamilyController.Test.cs
+++ XUnitTest/FamilyController.Test.cs
@@ -18,33 +18,48 @@ namespace XUnitTest
 {
     public class FamilyControllerTest
     {
-        /*https://docs.microsoft.com/en-us/aspnet/web-api/overview/testing-and-debugging/unit-testing-controllers-in-web-api#testing-actions-that-return-httpresponsemessage*/
+        /*https://docs.microsoft.com/en-us/aspnet/web-api/overview/testing-and-debugging/unit-testing-controllers-in-web-api#testing-actions-that-return-httpresponsemessage
+         https://stackoverflow.com/questions/50801094/how-to-get-the-values-from-a-taskiactionresult-returned-through-an-api-for-uni*/
 
         [Fact]
-        public void GetReturnsFamilyById_ExpectCorrectReturn()
+        public async Task GetReturnsFamilyById_ExpectCorrectIdAsync()
         {
             //Arrange
-            var dtoItem = GetItem();
+            var responseObject = GetItem();
 
             var mockRepository = new Mock<IFamilyRepository>();
-            mockRepository.Setup(x => x.GetFamilyById(1)).
-                Returns(dtoItem);
+            mockRepository.Setup(x => x.GetFamilyById(1))
+                .Returns(responseObject);
 
             var controller = new FamilyController(mockRepository.Object);
 
-            //Act
-            var testResult = controller.GetFamilyById(1);
+            //Act          
+            Task<IActionResult> actionResult = controller.GetFamilyById(1);
+            var contentResult = await actionResult as OkObjectResult;
+            var expectedObject = new IdNameDTO
+            {
+                Id = 1,
+                Name = "Phocidae"
+            };
 
             //Assert
-            Assert.NotNull(testResult);
-            Assert.Equal(1, testResult.Id);
+            Assert.NotNull(contentResult);
+            Assert.Equal(200, contentResult.StatusCode);
+
+            IdNameDTO actualObject = contentResult.Value as IdNameDTO;
+            Assert.Equal(expectedObject.Id, actualObject.Id);
+            Assert.Equal(expectedObject.Name, actualObject.Name);
         }
 
         private async Task<IdNameDTO> GetItem()
         {
-            var itemToBeReturned = new IdNameDTO { Id = 1, Name = "Phocidae" };
+            var itemToReturn = new IdNameDTO
+            {
+                Id = 1,
+                Name = "Phocidae"
+            };
 
-            return itemToBeReturned;
+            return itemToReturn;
         }
     }
 }
