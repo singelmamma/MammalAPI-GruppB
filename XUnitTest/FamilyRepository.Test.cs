d48aec28b3d15c8ffc6ac8ec5bd5a91e2b61c5f0
antonnorgren <56293438+AntonNorgren@users.noreply.github.com>
2020-05-14 18:11:23 +0200

    Added test for GetFamilyByName

--- XUnitTest/FamilyRepository.Test.cs
+++ XUnitTest/FamilyRepository.Test.cs
@@ -16,6 +16,57 @@ namespace XUnitTest
 {
     public class FamilyRepositoryTest
     {
+        [Fact]
+        public void GetFamilyByName_TwoTestFamilies_ExpectedNotNull()
+        {
+            // Arrange
+            var contextMock = new Mock<DBContext>();
+            contextMock.Setup(f => f.Families).ReturnsDbSet(GetTestFamilies());
+            var logger = Mock.Of<ILogger<FamilyRepository>>();
+            var familyRepository = new FamilyRepository(contextMock.Object, logger);
+            var name = "Test Family One";
 
+            var result = new Family
+            {
+                FamilyId = 1,
+                Name = "Test Family One",
+            };
+
+            // Act
+            var expected = familyRepository.GetFamilyByName(name);
+
+            // Assert
+            Assert.NotNull(expected.Result);
+            Assert.Equal(result.Name, expected.Result.Name);
+        }
+
+        
+
+        //public void GetFamilyById(int id)
+        //{
+
+        //}
+
+        //public void GetAllFamilies()
+        //{
+
+        //}
+
+        private List<Family> GetTestFamilies()
+        {
+            var sessions = new List<Family>();
+            sessions.Add(new Family()
+            {
+                FamilyId = 1,
+                Name = "Test Family One",
+
+            });
+            sessions.Add(new Family()
+            {
+                FamilyId = 2,
+                Name = "Test Family Two",
+            });
+            return sessions;
+        }
     }
 }
