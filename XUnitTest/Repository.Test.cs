30f89ed5758d021482315e29b8a507474b6b696e
Murat <m.kugic@gmail.com>
2020-05-19 10:41:10 +0200

    Added first repository test

--- XUnitTest/Repository.Test.cs
+++ XUnitTest/Repository.Test.cs
@@ -11,11 +11,60 @@ using MammalAPI.Context;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Moq.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace XUnitTest
 {
     public class RepositoryTest
     {
+        [Fact]
+        public void AddMammalWithRepositoryMethod_Add()
+        {
+            // Arrange
+            var mammals = TestMammals();
+            var dbContext = new Mock<DBContext>();
+            var logger = Mock.Of<ILogger<Repository>>();
 
+            var contextMock = new Mock<IRepository>();
+            contextMock.Setup(x => x.Add(new Mammal {}));
+
+            var repoMock = new Repository(dbContext.Object, logger);
+            
+
+            // Act
+            repoMock.Add(new Mammal 
+            {
+                MammalId = 3,
+                Name = "Big Willy",
+                LatinName = "Biggus Willus"
+            });
+
+            var saved = repoMock.Save();
+
+            var actual = TestMammals();
+
+            // Assert
+            // Assert.Equal(3, actual.Count);
+            Assert.True(saved.Result);
+        }
+
+        private List<Mammal> TestMammals()
+        {
+            var sessions = new List<Mammal>();
+            sessions.Add(new Mammal()
+            {
+                MammalId = 1,
+                Name = "Test Mammal One",
+                LatinName = "Bapa latin apa"
+            });
+            sessions.Add(new Mammal()
+            {
+                MammalId = 2,
+                Name = "Test Mammal Two",
+                LatinName = "Latin kanske med"
+            });
+
+            return sessions;
+        }
     }
 }
