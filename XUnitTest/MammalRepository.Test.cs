846e90189b4649f11117be2fe2e3f6cd02dea70d
Zorkii <56868345+aohzork@users.noreply.github.com>
2020-05-19 12:39:11 +0200

    MGB-96 Test MammalRepository Getall

--- XUnitTest/MammalRepository.Test.cs
+++ XUnitTest/MammalRepository.Test.cs
@@ -64,9 +64,6 @@ namespace XUnitTest
             var contextMock = new Mock<DBContext>();
             contextMock.Setup(m => m.Mammals).ReturnsDbSet(mammals);
 
-            //var contextMock = new Mock<IMammalRepository>();
-            //contextMock.Setup(m => m.GetAllMammals()).Returns(GetTestAllMammals());
-            
             var logger = Mock.Of<ILogger<MammalRepository>>();
             var mammalRepository = new MammalRepository(contextMock.Object, logger);
 
@@ -75,32 +72,43 @@ namespace XUnitTest
             var actualGetAll = await mammalRepository.GetAllMammals();
 
             // Assert
-            Assert.Equal(expectedDTO, actualGetAll);
+            Assert.NotNull(actualGetAll);          
+            Assert.Equal(typeof(List<MammalsDTO>), actualGetAll.GetType());
+
+            Assert.True(expectedDTO[0].MammalId.Equals(actualGetAll[0].MammalId));
+            Assert.True(expectedDTO[0].Name.Equals(actualGetAll[0].Name));
+            Assert.True(expectedDTO[0].LatinName.Equals(actualGetAll[0].LatinName));
+            Assert.True(expectedDTO[0].Length.Equals(actualGetAll[0].Length));
+            Assert.True(expectedDTO[0].Weight.Equals(actualGetAll[0].Weight));
         }
 
         private static IList<Mammal> GenerateMammals()
         {
-            List<Mammal> items = new List<Mammal>();
-            items.Add(new Mammal
+            return new List<Mammal>
             {
-                MammalId = 1,
-                Name = "Grey Whale",
-                LatinName = "Latin name1",
-                Length = 250,
-                Weight = 30000
-            });
-            items.Add(new Mammal
-            {
-                MammalId = 2,
-                Name = "Pink Whale",
-                LatinName = "Latin name2",
-                Length = 750,
-                Weight = 800000
-            });
-
-            //return Task.Run(() => items);
-
-            return items;
+                new Mammal
+                {
+                    MammalId = 1,
+                    Name = "Grey Whale",
+                    Length = 250,
+                    Weight = 30000,
+                    LatinName = "Latin name1",
+                    Lifespan = 100,
+                    Family = new Family
+                    {
+                        FamilyId = 1,
+                        Name = "Hoppoloppo"
+                    },
+                    MammalHabitats = new List<MammalHabitat>()
+                    {
+                        new MammalHabitat
+                        {
+                            HabitatId = 1,
+                            MammalId = 1
+                        }
+                    }                    
+                }
+            };
         }
 
         private static List<MammalsDTO> GenerateMammalsDTO()
@@ -114,18 +122,8 @@ namespace XUnitTest
                 Length = 250,
                 Weight = 30000
             });
-            items.Add(new MammalsDTO
-            {
-                MammalId = 2,
-                Name = "Pink Whale",
-                LatinName = "Latin name2",
-                Length = 750,
-                Weight = 800000
-            });
 
-            //return Task.Run(() => items);
-
-           return items;
+            return items;
         }
     }
 }
