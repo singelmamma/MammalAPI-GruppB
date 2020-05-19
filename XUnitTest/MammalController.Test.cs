24c7930df9e99e0fdf263a5e7cf5ad6345fb0663
Murat <m.kugic@gmail.com>
2020-05-19 09:23:10 +0200

    Added test for the mammalController GetMammalById

--- XUnitTest/MammalController.Test.cs
+++ XUnitTest/MammalController.Test.cs
@@ -1,21 +1,56 @@
-using System;
 using Xunit;
-using System.Collections.Generic;
-using System.Linq;
-using MammalAPI;
 using MammalAPI.Models;
 using MammalAPI.Controllers;
 using MammalAPI.Services;
 using Microsoft.AspNetCore.Mvc;
-using MammalAPI.Context;
-using Microsoft.Extensions.Logging;
 using Moq;
-using Moq.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace XUnitTest
 {
     public class MammalControllerTest
     {
+        [Fact]
+        public async Task GetMamalById_ReturnsMammalWithIdOne()
+        {
+            // Arrange
+            var responseObject = GetTestMammals();
 
+            var mockRepository = new Mock<IMammalRepository>();
+            mockRepository.Setup(m => m.GetMammalById(1))
+                .Returns(GetTestMammals());
+
+            var controller = new MammalController(mockRepository.Object);
+
+            // Act
+            Task<IActionResult> result = controller.GetMammalById(1);
+            var contentResult = await result as OkObjectResult;
+            var expected = new Mammal 
+            {
+                MammalId = 1,
+                Name = "Test Mammal One",
+                LatinName = "Bapa latin apa"
+            };
+
+            // Assert
+            Assert.NotNull(result);
+            
+            Mammal actual = contentResult.Value as Mammal;
+            Assert.Equal(expected.MammalId, actual.MammalId);
+            Assert.Equal(expected.Name, actual.Name);
+            Assert.Equal(expected.LatinName, actual.LatinName);
+        }
+
+        private async Task<Mammal> GetTestMammals()
+        {
+            var output = new Mammal
+            {
+                MammalId = 1,
+                Name = "Test Mammal One",
+                LatinName = "Bapa latin apa"
+            };
+
+            return output;
+        }
     }
 }
