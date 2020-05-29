8fdd883ba4ddd8f1ded5b051a6a0d305acaeffd3
MirkoPralica <56869159+MirkoPralica@users.noreply.github.com>
2020-05-29 13:27:51 +0200

    Removed the ursing dirr in startup

--- MammalAPI/Startup.cs
+++ MammalAPI/Startup.cs
@@ -1,16 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using MammalAPI.Context;
 using MammalAPI.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using AutoMapper;
-using MammalAPI.DTO;
 using Microsoft.OpenApi.Models;
 
 namespace MammalAPI
