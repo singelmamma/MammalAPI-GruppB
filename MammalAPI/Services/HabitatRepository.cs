6951671fcaecd737898f99b32827bd2f3f446d6e
singelmamma <55433931+singelmamma@users.noreply.github.com>
2020-05-29 13:17:18 +0200

    Update HabitatRepository.cs
    Cleaned up whitespaces and rearranged the order for GetMethods

--- MammalAPI/Services/HabitatRepository.cs
+++ MammalAPI/Services/HabitatRepository.cs
@@ -28,6 +28,21 @@ namespace MammalAPI.Services
             return await query.ToListAsync();
         }
 
+        public async Task<Habitat> GetHabitatById(int id, bool includeMammal = false)
+        {
+            _logger.LogInformation($"Getting habitat with id: { id }");
+            IQueryable<Habitat> query = _dBContext.Habitats.Where(x => x.HabitatID == id);
+
+            if (includeMammal == true)
+            {
+                query = query.Include(x => x.MammalHabitats).ThenInclude(x => x.Mammal);
+            }
+
+            if (query == null) throw new System.Exception($"Not found {id}");
+
+            return await query.FirstOrDefaultAsync();
+        }
+
         public async Task<Habitat> GetHabitatByName(string name, bool includeMammal=false)
         {
             _logger.LogInformation($"Getting habitat with name: { name }");
@@ -40,21 +55,6 @@ namespace MammalAPI.Services
 
             query = query.Where(c => c.Name == name);
             if (query == null) throw new System.Exception($"Not found {name}");
-            return await query.FirstOrDefaultAsync();
-        }
-
-        public async Task<Habitat> GetHabitatById(int id, bool includeMammal = false)
-        {
-            _logger.LogInformation($"Getting habitat with id: { id }");
-            IQueryable<Habitat> query = _dBContext.Habitats.Where(x => x.HabitatID ==id);
-
-            if (includeMammal==true)
-            {
-                query = query.Include(x => x.MammalHabitats).ThenInclude(x => x.Mammal);
-            }
-
-            if (query == null) throw new System.Exception($"Not found {id}");
-            
             return await query.FirstOrDefaultAsync();
         }
     }
