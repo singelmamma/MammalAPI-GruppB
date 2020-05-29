bdbe60ec2ac9521b18402e25f7a142effd049421
singelmamma <55433931+singelmamma@users.noreply.github.com>
2020-05-29 13:17:50 +0200

    D
    Deleted whitespaces and rearranged the order for all the tasks

--- MammalAPI/Services/IMammalRepository.cs
+++ MammalAPI/Services/IMammalRepository.cs
@@ -8,13 +8,14 @@ namespace MammalAPI.Services
 {
     public interface IMammalRepository : IRepository
     {
-        Task<List<Mammal>> GetMammalByName(string mammalName, bool includeFamilies = false);
-        Task<List<Mammal>> GetMammalsByHabitatId(int id, bool includeFamily = false, bool includeHabitat = false);
         Task<List<Mammal>> GetAllMammals(bool includeFamily = false, bool includeHabitat = false);
         Task<Mammal> GetMammalById(int id, bool includeFamily = false, bool includeHabitat = false);
-        Task<List<Mammal>> GetMammalsByHabitat(string habitatName);
+        Task<List<Mammal>> GetMammalByName(string mammalName, bool includeFamilies = false);  
         Task<List<Mammal>> GetMammalsByLifeSpan(int fromYear, int toYear, bool includeFamily = false, bool includeHabitat = false);
-        Task<List<Mammal>> GetMammalsByFamily(string familyName, bool includeHabitat = false, bool includeFamily = false);
+        Task<List<Mammal>> GetMammalsByHabitatId(int id, bool includeFamily = false, bool includeHabitat = false);
+        Task<List<Mammal>> GetMammalsByHabitat(string habitatName);
         Task<List<Mammal>> GetMammalsByFamilyId(int id, bool includeHabitat = false, bool includeFamily = false);
+        Task<List<Mammal>> GetMammalsByFamily(string familyName, bool includeHabitat = false, bool includeFamily = false);
+        
     }
 }
