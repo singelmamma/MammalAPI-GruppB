32ba8113959da250d9d42048660c34d715f84b8e
singelmamma <55433931+singelmamma@users.noreply.github.com>
2020-06-03 13:29:39 +0200

    MGB-242 Added useraccount database table

---
+++ MammalAPI/Migrations/20200603112805_addeduseraccounttable.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MammalAPI.Migrations
+{
+    public partial class addeduseraccounttable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserAccounts",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserAccounts", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserAccounts");
+        }
+    }
+}
---
+++ MammalAPI/Models/UserAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MammalAPI.Models
+{
+    public class UserAccount
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
