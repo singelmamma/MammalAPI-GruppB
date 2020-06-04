ccc604c273ed9990edf4ad24d67137f238e2ed8a
singelmamma <55433931+singelmamma@users.noreply.github.com>
2020-06-04 13:57:01 +0200

    MGB-245 added fields for headers in swagger
    @singelmamma @mirkopralica

---
+++ MammalAPI/Authentication/HeaderParameterSwagger.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MammalAPI.Authentication
+{
+    
+    public class HeaderParameterSwagger : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            if (operation.Parameters == null) operation.Parameters = new List<OpenApiParameter>();
+
+            var descriptor = context.ApiDescription.ActionDescriptor as ControllerActionDescriptor;
+
+           
+
+            if(descriptor != null && context.ApiDescription.HttpMethod.Equals("POST") || context.ApiDescription.HttpMethod.Equals("PUT") || context.ApiDescription.HttpMethod.Equals("DELETE"))
+            {
+                operation.Parameters.Add(new OpenApiParameter
+                {
+                    Name = "APIUsername",
+                    In = ParameterLocation.Header,                  
+                    Required = true
+                   
+                });
+
+                operation.Parameters.Add(new OpenApiParameter
+                {
+                    Name = "APIPassword",
+                    In = ParameterLocation.Header,
+                    Required = true
+                });
+            }
+        }
+    }
+}
