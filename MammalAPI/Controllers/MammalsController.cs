06714903f8af4f4e6dd0ba706aa28bc7f224963b
Gerglamesh <hampe.kjellstrand@gmail.com>
2020-06-05 11:33:13 +0200

    @Gerglamesh solved bug in GetMammalByFamilyName where including families threw exception

--- MammalAPI/Controllers/MammalsController.cs
+++ MammalAPI/Controllers/MammalsController.cs
@@ -274,17 +274,6 @@ namespace MammalAPI.Controllers
                 IEnumerable<MammalDTO> mappedResult = _mapper.Map<MammalDTO[]>(results);
                 Dictionary<string, FamilyDTO> items = new Dictionary<string, FamilyDTO>();
 
-                if (includeFamily)
-                {
-                    foreach (MammalDTO mammal in mappedResult)
-                    {
-                        if (mammal.Family != null)
-                        {
-                            mammal.Family.Mammals = items[mammal.Family.Name].Mammals;
-                        }
-                    }
-                }
-
                 if (includeLinks)
                 {
                     foreach (var mammal in mappedResult)
@@ -303,6 +292,16 @@ namespace MammalAPI.Controllers
                             mammal.Family.Mammals = null;
                         }
                     }
+
+                    foreach (MammalDTO mammal in mappedResult)
+                    {
+                        if (mammal.Family != null)
+                        {
+                            mammal.Family.Mammals = items[mammal.Family.Name].Mammals;
+                        }
+                    }                    
+
+                    mappedResult = mappedResult.Select(m => HateoasMainLinks(m));
                 }
                 
                 return Ok(mappedResult);
